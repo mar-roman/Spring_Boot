a19add69855825d19763e05b09325470451e8d82
mar-roman <83491762+mar-roman@users.noreply.github.com>
2021-12-05 22:32:04 +0300

    create spring_boot

--- src/main/java/com/marroman/example/core/CoreApplication.java
+++ src/main/java/com/marroman/example/core/CoreApplication.java
@@ -1,4 +1,4 @@
-package com.dronikon.example.core;
+package com.marroman.example.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
